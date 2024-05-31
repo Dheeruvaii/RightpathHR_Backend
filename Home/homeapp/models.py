f266f1ef40b4983473e9cd0723f46a34fc870f3a
dheeruvaii <dhirajsapkota260@gmail.com>
2024-05-31 11:19:50 +0545

    update destination models and serializers

--- Home/homeapp/models.py
+++ Home/homeapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Destination(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    image_url = models.URLField()
+    image_url = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
--- Home/homeapp/urls.py
+++ Home/homeapp/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+from rest_framework.routers import DefaultRouter
+from .views import DestinationViewSet
+
+router = DefaultRouter()
+router.register(r'destinations', DestinationViewSet)
+
+urlpatterns = router.urls
+# urlpatterns = [
+    
+#     path('admin/', admin.site.urls),
+# ]
